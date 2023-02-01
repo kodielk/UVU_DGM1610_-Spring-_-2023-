13d3928aeccd9653f37aa329451dc9bd344686af
Elise Cobabe <122411951+kodielk@users.noreply.github.com>
2023-01-31 18:43:30 -0700

    Changed variable

--- unit-1c/ForLoopsWithArraysLab.cs
+++ unit-1c/ForLoopsWithArraysLab.cs
@@ -6,7 +6,7 @@ public class Program
 	{
 		string[] playerTypes = {"Pawn", "Pawn", "Pawn",};
 		for (var i = 0; i < playerTypes.Length; i++){
-			playerTypes[i] = "Queen";
+			playerTypes[i] = "Rook";
 		}
 		
 		foreach(var playerType in playerTypes) {
