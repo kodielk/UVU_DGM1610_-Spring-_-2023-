9e9dd9df6122069edd2dc706a31d4f8d9ca32e92
Elise Cobabe <122411951+kodielk@users.noreply.github.com>
2023-02-01 20:37:11 -0700

    added zombies and playercount

--- unit-1c/Challenge.cs
+++ unit-1c/Challenge.cs
@@ -5,8 +5,14 @@ public class Program
 	public static void Main()
 	{
 		string[] classTypes = {"peasant", "peasant", "peasant", "king"};
+		
+		for(var i = 0; i < classTypes.Length; i++){
+			classTypes[i] = "Zombie";
+		}
+			
 		foreach (var classType in classTypes){
 			Console.WriteLine(classType);
 		}
+		Console.WriteLine("There are " + classTypes.Length + " people still alive.");
 	}
 }
