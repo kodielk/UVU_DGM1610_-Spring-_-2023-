f90172c9259cb4978e444b06386f6f37b6654816
Elise Cobabe <122411951+kodielk@users.noreply.github.com>
2023-02-01 20:12:27 -0700

    added some complexity to the code

--- unit-1c/ForeachLoopsWithArraysLab.cs
+++ unit-1c/ForeachLoopsWithArraysLab.cs
@@ -5,7 +5,7 @@ public class Program
 	public static void Main()
 	{
 		string[] champions = {"Swain", "Galio", "Annie", "Thresh", "Rek'Sai", "Vi"};
-		Console.WriteLine(champions.Length);
+		Console.WriteLine("I can play " + champions.Length + " champions.");
 		foreach(var champion in champions){
 			Console.WriteLine("I love playing " + champion + ".");
 		}
