84bbf1832fa166b4dce4ee16cc28edde4ad711b3
Elise Cobabe <10920472@uvu.edu>
2023-02-14 18:09:10 -0700

    added a cubecolor

---
+++ unit-2/Spring 2023/Assets/cubecolor.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class CubeColor : MonoBehaviour
+{
+
+	void update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			GetComponent<Renderer> ().material.color =  Color.red;
+		}
+		if (Input.GetKeyDown(KeyCode.G)){
+			GetComponent<Renderer> ().material.color = Color.green;
+		}
+		if (Input.GetKeyDown(KeyCode.B)){
+			GetComponent<Renderer> ().material.color = Color.blue;
+		}
+	}
+}
